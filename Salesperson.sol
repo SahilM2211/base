88427872d75c42f753201e944c3345e0f06ef64f
SahilM2211 <122855735+SahilM2211@users.noreply.github.com>
2025-10-16 22:20:08 +0530

    Add files via upload

---
+++ Salesperson.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./Hourly.sol";
+
+contract Salesperson is Hourly {
+    constructor(uint256 _idNumber, uint256 _managerId, uint256 _hourlyRate)
+        Hourly(_idNumber, _managerId, _hourlyRate)
+    {}
+}
